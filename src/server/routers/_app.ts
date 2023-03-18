824d21b8413bb4ec4ed88861d90e854d3f5ee54b
Nik Tripp <nik.r.tripp@gmail.com>
2023-03-17 18:13:53 -0700

    Running basics, just needs style

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,12 +2,12 @@
  * This file contains the root router of your tRPC-backend
  */
 import { publicProcedure, router } from '../trpc';
-import { postRouter } from './post';
+import { shipmentRouter } from './shipment';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
-  post: postRouter,
+  shipment: shipmentRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/routers/shipment.ts
@@ -0,0 +1,63 @@
+/**
+ *
+ * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
+ */
+import { router, publicProcedure } from '../trpc';
+import { Prisma } from '@prisma/client';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import { prisma } from '~/server/prisma';
+
+/**
+ * Default selector for Post/Put/Patch.
+ * It's important to always explicitly say which fields you want to return in order to not leak extra information
+ * @see https://github.com/prisma/prisma/issues/9353
+ */
+const shipmentUpdateArgs = Prisma.validator<Prisma.KitShipmentSelect>()({
+  labelId: true,
+  shippingTrackingCode: true,
+});
+
+export const shipmentRouter = router({
+  byId: publicProcedure.input(z.object({id: z.number()})).query(async ({input}) => {
+      return prisma.kitShipment.findUnique({ where: input });
+    }),
+  byLabelId: publicProcedure
+    .input(
+      z.object({
+        labelId: z.string(),
+      }),
+    )
+    .query(async ({ input }) => {
+      return prisma.kitShipment.findUnique({
+        where: input,
+      });
+    }),
+  add: publicProcedure
+    .input(
+      z.object({
+        labelId: z.string(),
+        shippingTrackingCode: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      return prisma.kitShipment.create({
+        data: input,
+      });
+    }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        labelId: z.string().optional(),
+        shippingTrackingCode: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { id, ...data } = input;
+      return prisma.kitShipment.update({
+        where: { id },
+        data,
+      });
+    }),
+});
