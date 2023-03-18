06a4242744507375c313e012fdcb378a33b005c5
Nik Tripp <nik.r.tripp@gmail.com>
2023-03-17 19:00:27 -0700

    We do a little styling here

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next';
-import type { AppType, AppProps } from 'next/app';
-import type { ReactElement, ReactNode } from 'react';
-import { DefaultLayout } from '~/components/DefaultLayout';
-import { trpc } from '~/utils/trpc';
+import type { NextPage } from "next";
+import type { AppType, AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
+import { DefaultLayout } from "~/components/DefaultLayout";
+import { trpc } from "~/utils/trpc";
+import { MantineProvider } from "@mantine/core";
 
-export type NextPageWithLayout<
-  TProps = Record<string, unknown>,
-  TInitialProps = TProps,
-> = NextPage<TProps, TInitialProps> & {
+export type NextPageWithLayout<TProps = Record<string, unknown>, TInitialProps = TProps> = NextPage<
+  TProps,
+  TInitialProps
+> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
@@ -16,10 +17,22 @@ type AppPropsWithLayout = AppProps & {
 };
 
 const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
-  const getLayout =
-    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
+  const getLayout = Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <MantineProvider
+      theme={{
+        globalStyles: (theme) => ({
+          body: {
+            backgroundColor: "#0d2d4c",
+          },
+        }),
+      }}
+      withGlobalStyles
+    >
+      <Component {...pageProps} />
+    </MantineProvider>,
+  );
 }) as AppType;
 
 export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { trpc } from '../utils/trpc';
-import { NextPageWithLayout } from './_app';
-import { Button, Container, MantineProvider, TextInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
-import { useState } from 'react';
+import { trpc } from "../utils/trpc";
+import { NextPageWithLayout } from "./_app";
+import { Button, Card, Container, Stack, TextInput, Title, Text, Anchor } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
 
 const IndexPage: NextPageWithLayout = () => {
-  const formController = useForm({ initialValues: { labelId: '' } });
-  const [searchLabel, setSearchLabel] = useState('');
+  const formController = useForm({ initialValues: { labelId: "" } });
+  const [searchLabel, setSearchLabel] = useState("");
   const { data: retrievedShipment } = trpc.shipment.byLabelId.useQuery(formController.values, {
     enabled: Boolean(searchLabel),
   });
@@ -20,15 +20,34 @@ const IndexPage: NextPageWithLayout = () => {
   // }, [postsQuery.data, utils]);
 
   return (
-    <MantineProvider>
-      <Container>
-        <form onSubmit={formController.onSubmit((values) => setSearchLabel(values.labelId))}>
-          <TextInput label="Kit ID" {...formController.getInputProps('labelId')} />
-          <Button type="submit">Find my Shipment Info</Button>
-        </form>
-      </Container>
-      {retrievedShipment && 'Located shipment'}
-    </MantineProvider>
+    <Container sx={{ height: "100vh" }}>
+      <Card p="xl">
+        <Stack>
+          <Title>Track your shipment</Title>
+          <Text>Enter your kit ID to find your tracking information</Text>
+          <form onSubmit={formController.onSubmit((values) => setSearchLabel(values.labelId))}>
+            <Stack>
+              <TextInput label="Kit ID" {...formController.getInputProps("labelId")} />
+              <Button type="submit">Find my Shipment Info</Button>
+            </Stack>
+          </form>
+          {Boolean(searchLabel) &&
+            (retrievedShipment ? (
+              <>
+                <Text>Your tracking ID is {retrievedShipment.shippingTrackingCode}</Text>
+                <Anchor
+                  href={`https://www.fedex.com/fedextrack/?trknbr=${retrievedShipment.shippingTrackingCode}`}
+                  target="_blank"
+                >
+                  Track your shipment with FedEx
+                </Anchor>
+              </>
+            ) : (
+              <Text>Sorry, we didn&apos;t find anything for that kit label</Text>
+            ))}
+        </Stack>
+      </Card>
+    </Container>
   );
 };
 
